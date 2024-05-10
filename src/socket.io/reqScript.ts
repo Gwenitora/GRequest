ed36c495ae9055e32b58aa2c6762832310175da0
Gwenitora <gwenitora.gardu74@gmail.com>
2024-05-10 15:34:52 +0200

    socketio file

--- src/socket.io/reqScript.ts
+++ src/socket.io/reqScript.ts
@@ -1,20 +1,20 @@
 import { requ, Request } from "@gscript/grequest";
-import { json_template, json_type } from "@gscript/gtools/lib/cjs/json/type";
+import { json } from "@gscript/gtools";
 
 export class ReqScriptSocketIo extends Request {
-    link: string = "/socketio";
+    link: string = "/socketio/:file";
     type: requ.type = requ.type.PUBLIC;
     callType: requ.callType = requ.callType.GET;
     authLevel: string | boolean = true;
-    inTemplates: json_template[] = [];
-    outTemplates: json_template[] = [];
+    inTemplates: json.template[] = [];
+    outTemplates: json.template[] = [];
     secret: boolean = false;
 
     start(): boolean {
         return true;
     }
     
-    async run(template: number, body: json_type, header: { [x: string]: string; }, linkVar: { [x: string]: string; }, query: { [x: string]: string; }): Promise<{ resBody: json_type; resCode: requ.httpCodes.all; }> {
-        return {resBody: {message: "Hello World"}, resCode: requ.httpCodes._200_Success._200_OK};
+    async run(template: number, body: json.type, header: { [x: string]: string; }, linkVar: { [x: string]: string; }, query: { [x: string]: string; }): Promise<{ resFile: string; resCode: requ.httpCodes.all; }> {
+        return {resFile: "./node_modules/socket.io/client-dist/" + query.file, resCode: requ.httpCodes._200_Success._200_OK};
     }
 }
